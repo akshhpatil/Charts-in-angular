67053cd78c97efe34487a1004ef7cfffaa8924be
Akshay Patil <76992034+akshhpatil@users.noreply.github.com>
2021-03-02 08:09:47 +0530

    Create database.php

---
+++ php backend/chart/database.php
@@ -0,0 +1,17 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header("Content-Type:application/json;charset=UTF-8");
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "ang";
+
+// Create connection
+$con = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($con->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
