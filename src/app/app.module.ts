16ad3c313879420a96d665968c543173cac94b90
Akshay Patil <76992034+akshhpatil@users.noreply.github.com>
2021-03-02 08:09:01 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
+import { ChartsModule  } from "ng2-charts";
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewComponent } from './new/new.component';
+import { HomeComponent } from './home/home.component';
+import { BarchartComponent } from './barchart/barchart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewComponent,
+    HomeComponent,
+    BarchartComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, HttpClientModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
