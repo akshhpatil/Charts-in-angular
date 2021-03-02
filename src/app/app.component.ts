35437ae93b940ec4f2cd5f5b6ae7a4b6729b7205
Akshay Patil <76992034+akshhpatil@users.noreply.github.com>
2021-03-02 08:09:07 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'charts';
+  title = 'charts in angular'
+
+ 
+ constructor(  ){}
+
+ 
+ ngOnInit()
+  {
+    
+}
+
+
 }
