f2e7dbe8c5896cc91f970002f35f321f3fafc2d5
Akshay Patil <76992034+akshhpatil@users.noreply.github.com>
2021-03-04 17:16:39 +0530

    Update new.component.ts

--- src/app/new/new.component.ts
+++ src/app/new/new.component.ts
@@ -4,17 +4,12 @@ import { ChartDataSets, ChartOptions } from 'chart.js';
 import { Color, Label } from 'ng2-charts';
 import { Element } from  '../element';
 
-
-
 @Component({
   selector: 'app-new',
   templateUrl: './new.component.html',
   styleUrls: ['./new.component.css']
 })
 export class NewComponent implements OnInit {
-
-  
-
 
   url='http://localhost/chart/read.php';
 
@@ -23,7 +18,7 @@ export class NewComponent implements OnInit {
   id=[];
   number=[];
   month=[];  
- result:Element[]
+  result:Element[]
 
   constructor(private http:HttpClient) { }
 
@@ -31,8 +26,6 @@ export class NewComponent implements OnInit {
   {
     this.http.get(this.url).subscribe((result:Element[]) => {  
       result.forEach(x => {  
-       // this.month.push(x.month);  
-       // this.number.push(x.number);
         this.result = result;
         console.log(this.result);
       }); 
